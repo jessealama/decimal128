7c63f3511f4b0923960378835b21449f13ed3a10
Jesse Alama <jesse@igalia.com>
2023-05-02 14:06:19 +0200

    Combine tests into a single data structure

--- tests/decimal128.test.ts
+++ tests/decimal128.test.ts
@@ -231,42 +231,25 @@ describe("multiply", () => {
 });
 
 describe("divide", () => {
-    test("finite decimal representation", () => {
-        expect(
-            new Decimal128("0.654")
-                .divide(new Decimal128("0.12"))
-                .equals(new Decimal128("5.45"))
-        );
-    });
-    test("infinite decimal representation", () => {
-        expect(
-            new Decimal128("0.11")
-                .divide(new Decimal128("0.3"))
-                .equals(new Decimal128("0.36666666666666666666666"))
-        );
-    });
-    test("many digits, few significant", () => {
-        expect(
-            new Decimal128("0.00000000000000000000000000000000000001")
-                .divide(new Decimal128("2"))
-                .equals(
-                    new Decimal128("0.000000000000000000000000000000000000005")
-                )
-        );
-    });
+    let tests = {
+        "finite decimal representation": ["0.654", "0.12", "5.45"],
+        "infinite decimal representation": ["0.11", "0.3", "0.36666666666666666666666"],
+        "many digits, few significant": ["0.00000000000000000000000000000000000001", "2", "0.000000000000000000000000000000000000005"],
+        "one third": ["1", "3", "0." + "3".repeat(MAX_SIGNIFICANT_DIGITS)],
+    }
+    for (let [name, [a, b, c]] of Object.entries(tests)) {
+        test(name, () => {
+            expect(
+                new Decimal128(a)
+                    .divide(new Decimal128(b))
+                    .toString()
+            ).toStrictEqual(c);
+        });
+    }
     test("divide by zero", () => {
         expect(() =>
             new Decimal128("123.456").divide(new Decimal128("0.0"))
         ).toThrow(RangeError);
-    });
-    test("one third", () => {
-        expect(
-            one
-                .divide(new Decimal128("3"))
-                .equals(
-                    new Decimal128("0." + "3".repeat(MAX_SIGNIFICANT_DIGITS))
-                )
-        );
     });
 });
 
