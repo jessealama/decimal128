9894579eacc10a7f7e75c5fd305ac41226130d3d
Jesse Alama <jesse@igalia.com>
2023-07-17 13:31:27 +0200

    Look for JavaScript files, too

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
+    runner: "jest-light-runner",
     transform: { "^.+\\.ts?$": "ts-jest" },
     testEnvironment: "node",
-    testRegex: "/tests/.*\\.(test|spec)?\\.(ts|tsx)$",
+    testRegex: "/tests/.*\\.(test|spec)?\\.(js|ts|tsx)$",
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     coverageThreshold: {
         global: {
