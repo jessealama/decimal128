9963039295fece1fcac423f89553ab65ba06737b
Jesse Alama <jesse@igalia.com>
2024-01-24 11:44:54 +0100

    Use correct class name

--- examples/step.js
+++ examples/step.js
@@ -6,9 +6,9 @@ const ten = new Decimal128("10");
 
 function stepUp(d, n, x) {
     let increment = pow(ten, x);
-    return d.add(n.times(increment));
+    return d.add(n.multiply(increment));
 }
 
 let starting = new Decimal128("1.23");
-let stepped = stepUp(starting, new Decimal128("3"), new Decimal("-4"));
+let stepped = stepUp(starting, new Decimal128("3"), new Decimal128("-4"));
 console.log(stepped.toString({ numDecimalDigits: 4 })); // 1.2305
