bfd8c85a779ade24775e6a6f233793a6496d0e23
Amos Wong <amoswwj@gmail.com>
2024-04-10 17:40:12 +0800

    chore: configure jest to work with mts file

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,26 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    runner: "jest-light-runner",
-    transform: { "^.+\\.ts?$": "ts-jest" },
+    preset: 'ts-jest/presets/default-esm',
+    transform: {
+        '^.+\\.m?[tj]sx?$': [
+            'ts-jest',
+            {
+                useESM: true,
+            },
+        ],
+    },
+    extensionsToTreatAsEsm: ['.mts', '.ts'],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    collectCoverageFrom: [
+        "src/**/*.mts",
+    ],
     testEnvironment: "node",
     testRegex: "/tests/.*\\.(test|spec)?\\.(js|ts|tsx)$",
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    moduleFileExtensions: ['js', 'ts', 'mts'],
+    resolver: "<rootDir>/mjs-resolver.cjs", 
+    coverageProvider: 'v8',
     coverageThreshold: {
         global: {
             lines: 98,
