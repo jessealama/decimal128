ea85dacca4d403515c7b80f9d510e6edc4f17750
amoshydra <8733840+amoshydra@users.noreply.github.com>
2025-01-07 01:14:03 +0800

    test(toprecision): include data that will result in rounding

--- tests/Decimal128/toprecision.test.js
+++ tests/Decimal128/toprecision.test.js
@@ -97,3 +97,149 @@ describe("infinity", () => {
         expect(s).toStrictEqual(o);
     });
 });
+
+describe("tests", () => {
+    describe("with integer output", () => {
+        test.each`
+            input      | digits     | output
+            ${" 0.0"}  |  ${1}      |  ${"0"}
+            ${" 1.4"}  |  ${1}      |  ${"1"}
+            ${" 1.5"}  |  ${1}      |  ${"2"}  // halfEven rounded up to even
+            ${" 1.6"}  |  ${1}      |  ${"2"}
+            ${" 2.4"}  |  ${1}      |  ${"1"}
+            ${" 2.5"}  |  ${1}      |  ${"2"}  // halfEven rounded down to even
+            ${" 2.6"}  |  ${1}      |  ${"3"}
+            ${"-0.0"}  |  ${1}      | ${"-0"}
+            ${"-1.4"}  |  ${1}      | ${"-1"}
+            ${"-1.5"}  |  ${1}      | ${"-2"}  // halfEven rounded up to even
+            ${"-1.6"}  |  ${1}      | ${"-2"}
+            ${"-2.4"}  |  ${1}      | ${"-1"}
+            ${"-2.5"}  |  ${1}      | ${"-2"}  // halfEven rounded down to even
+            ${"-2.6"}  |  ${1}      | ${"-3"}
+        `("$input precision($digits) = $output", ({ input, digits, output }) => {
+            const s = new Decimal128(input.trim()).toPrecision({ digits });
+            expect(s).toStrictEqual(output.trim());
+        });
+    });
+    describe("with decimal output", () => {
+        test.each`
+            input      | digits     | output
+            ${" 0.14"} |  ${2}      |  ${"0.1"}  
+            ${" 0.15"} |  ${2}      |  ${"0.2"}  // halfEven rounded up to even
+            ${" 0.16"} |  ${2}      |  ${"0.2"}
+            ${" 0.24"} |  ${2}      |  ${"0.2"}  
+            ${" 0.25"} |  ${2}      |  ${"0.2"}  // halfEven rounded down to even
+            ${" 0.26"} |  ${2}      |  ${"0.3"}
+            ${"-0.14"} |  ${2}      | ${"-0.1"}  
+            ${"-0.15"} |  ${2}      | ${"-0.2"}  // halfEven rounded up to even
+            ${"-0.16"} |  ${2}      | ${"-0.2"}
+            ${"-0.24"} |  ${2}      | ${"-0.2"}  
+            ${"-0.25"} |  ${2}      | ${"-0.2"}  // halfEven rounded down to even
+            ${"-0.26"} |  ${2}      | ${"-0.3"}
+        `("$input precision($digits) = $output", ({ input, digits, output }) => {
+            const s = new Decimal128(input.trim()).toPrecision({ digits });
+            expect(s).toStrictEqual(output.trim());
+        });
+    });
+    describe("with exponent output", () => {
+        test.each`
+            input      | digits     | output
+            ${" 1014"} |  ${3}      |  ${"101e+1"}  
+            ${" 1015"} |  ${3}      |  ${"102e+1"}  // halfEven rounded up to even
+            ${" 1016"} |  ${3}      |  ${"102e+1"}
+            ${" 1024"} |  ${3}      |  ${"102e+1"}  
+            ${" 1025"} |  ${3}      |  ${"102e+1"}  // halfEven rounded down to even
+            ${" 1026"} |  ${3}      |  ${"103e+1"}
+            ${"-1014"} |  ${3}      | ${"-101e+1"}  
+            ${"-1015"} |  ${3}      | ${"-102e+1"}  // halfEven rounded up to even
+            ${"-1016"} |  ${3}      | ${"-102e+1"}
+            ${"-1024"} |  ${3}      | ${"-102e+1"}  
+            ${"-1025"} |  ${3}      | ${"-102e+1"}  // halfEven rounded down to even
+            ${"-1026"} |  ${3}      | ${"-103e+1"}
+        `("$input precision($digits) = $output", ({ input, digits, output }) => {
+            const s = new Decimal128(input.trim()).toPrecision({ digits });
+            expect(s).toStrictEqual(output.trim());
+        });
+    });
+    describe("with large positive exponent output", () => {
+        const d = new Decimal128("1002500000_0005500000_0008500000_0001");
+        //                       "1234567890_1234567890_1234567890_1234"
+
+        describe("positive", () => {
+            test.each`
+                digits     | output
+                 ${3}      |  ${"100e+31"}
+                 ${4}      |  ${"1002e+30"}
+                 ${5}      |  ${"10025e+29"}
+                ${13}      |  ${"1002500000000e+21"}
+                ${14}      |  ${"10025000000006e+20"}
+                ${15}      |  ${"100250000000055e+19"}
+                ${23}      |  ${"10025000000005500000001e+11"}
+                ${24}      |  ${"100250000000055000000008e+10"}
+                ${25}      |  ${"1002500000000550000000085e+9"}
+            `("precision($digits) = $output", ({ digits, output }) => {
+                const s = d.toPrecision({ digits });
+                expect(s).toStrictEqual(output.trim());
+            });
+        });
+        describe("negative", () => {
+            const negD = d.negate();
+            test.each`
+                digits     | output
+                 ${3}      |  ${"-100e+31"}
+                 ${4}      |  ${"-1002e+30"}
+                 ${5}      |  ${"-10025e+29"}
+                ${13}      |  ${"-1002500000000e+21"}
+                ${14}      |  ${"-10025000000006e+20"}
+                ${15}      |  ${"-100250000000055e+19"}
+                ${23}      |  ${"-10025000000005500000001e+11"}
+                ${24}      |  ${"-100250000000055000000008e+10"}
+                ${25}      |  ${"-1002500000000550000000085e+9"}
+            `("precision($digits) = $output", ({ digits, output }) => {
+                const s = negD.toPrecision({ digits });
+                expect(s).toStrictEqual(output.trim());
+            });
+        });
+    });
+
+    describe("with large negative exponent output", () => {
+        const d = new Decimal128("0.1002500000_0005500000_0008500000_0001");
+        //                       "0.1234567890_1234567890_1234567890_1234"
+
+        describe("positive", () => {
+            test.each`
+                digits     | output
+                 ${3}      |  ${"0.100"}
+                 ${4}      |  ${"0.1002"}
+                 ${5}      |  ${"0.10025"}
+                ${13}      |  ${"0.1002500000000"}
+                ${14}      |  ${"0.10025000000006"}
+                ${15}      |  ${"0.100250000000055"}
+                ${23}      |  ${"0.10025000000005500000001"}
+                ${24}      |  ${"0.100250000000055000000008"}
+                ${25}      |  ${"0.1002500000000550000000085"}
+            `("precision($digits) = $output", ({ digits, output }) => {
+                const s = d.toPrecision({ digits });
+                expect(s).toStrictEqual(output.trim());
+            });
+        });
+        describe("negative", () => {
+            const negD = d.negate();
+            test.each`
+                digits     | output
+                 ${3}      |  ${"-0.100"}
+                 ${4}      |  ${"-0.1002"}
+                 ${5}      |  ${"-0.10025"}
+                ${13}      |  ${"-0.1002500000000"}
+                ${14}      |  ${"-0.10025000000006"}
+                ${15}      |  ${"-0.100250000000055"}
+                ${23}      |  ${"-0.10025000000005500000001"}
+                ${24}      |  ${"-0.100250000000055000000008"}
+                ${25}      |  ${"-0.1002500000000550000000085"}
+            `("precision($digits) = $output", ({ digits, output }) => {
+                const s = negD.toPrecision({ digits });
+                expect(s).toStrictEqual(output.trim());
+            });
+        });
+    });
+});
